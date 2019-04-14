bf24728af1ac51ebaec8bf8922e04943f66b7017
Daiki <nojiri1098@gmail.com>
2019-04-14 10:27:08 +0900

    Add copy

--- ga-knapsack.py
+++ ga-knapsack.py
@@ -1,61 +1,126 @@
-# -*- coding: utf-8 -*-
+# coding: utf-8
+
 import random
-import numpy as np
+import math
+import copy
+import operator
+import pandas as pd
 
-MAX_WEIGHT = 60
-POPULATION_COUNT = 50
-GENERATION_COUNT = 30000
-CROSSING_RATE = 0.8
-MUTATION_RATE = 0.3
-TYPE_WEIGHT = 1
-TYPE_PRICE = 2
+N_ITEMS = 20
+N_POP = 20
+N_GEN = 25
+MUTATE_PROB = 0.1
+ELITE_RATE = 0.5
 
-weights = [
-    9, 7, 8, 2, 10, 7, 7, 8, 5, 4, 7, 5, 7, 5, 9, 9, 9, 8, 8, 2, 7, 7, 9, 8, 4, 7,
-    3, 9, 7, 7, 9, 5, 10, 7, 10, 10, 7, 10, 10, 10, 3, 8, 3, 4, 2, 2, 5, 3, 9, 2
-]
-prices = [
-    20, 28, 2, 28, 15, 28, 21, 7, 28, 12, 21, 4, 31, 28, 24, 36, 33, 2, 25, 21, 35, 14, 36, 25, 12,
-    14, 40, 36, 2, 28, 33, 40, 22, 2, 18, 22, 14, 22, 15, 22, 40, 7, 4, 21, 21, 28, 40, 4, 24, 21
-]
+class GA:
+    def __init__(self):
+        self.items = {}
+        self.fitness_master = {}
 
-# 母集団を生成する
-def make_population():
-    populations = []
+    def main(self): 
+        pop = [{'param': p} for p in self.get_population()]
+        
+        for g in range(N_GEN):
+            print('Generation%3s:') % str(g), 
 
-    for i in range(0, POPULATION_COUNT):
-        individual = np.random.randint(0, 2, 50)
-        populations.append(individual) 
+            # Get elites
+            fitness = self.evaluate(pop)
+            elites = fitness[:int(len(pop)*ELITE_RATE)]
 
-    return populations
+            # Cross and mutate
+            pop = elites[:]
+            while len(pop) < N_POP:
+                if random.random() < MUTATE_PROB:
+                    m = random.randint(0, len(elites)-1)
+                    child = self.mutate(elites[m]['param'])
+                else:
+                    c1 = random.randint(0, len(elites)-1)
+                    c2 = random.randint(0, len(elites)-1)
+                    child = self.crossover(elites[c1]['param'], elites[c2]['param'])
+                pop.append({'param': child})
+            
+            # Evaluate indivisual
+            fitness = self.evaluate(pop)
+            pop = fitness[:]
 
-def evaluate(populations):
-    values = []
+            print(pop[0]['score0'], pop[0]['score1'], pop[0]['param'])
 
-    for individual in populations:
-        total_weight = calc_total_value(individual, TYPE_WEIGHT)
+            
+    def get_population(self):
+        # Make items
+        for i in xrange(N_ITEMS):
+            self.items[i] = (random.randint(0, 100), random.randint(1, 10))  # value, weight
 
-        if total_weight <= MAX_WEIGHT:
-            values.append(calc_total_value(individual, TYPE_PRICE))
-        else:
-            values.append(1)
+            print(self.items)
+            exit()
 
-    return values
+        # Make population
+        pop = []
+        for i in range(N_POP):
+            ind = [self.items[k] for k in random.sample(range(N_ITEMS), 5)]
+            pop.append(ind)
 
-def calc_total_value(array, type):
-    sum = 0
+        return pop
 
-    if type == TYPE_WEIGHT:
-        for i in array:
-            if i:
-                sum += weights[i]
-    else:
-        for i in array:
-            if i:
-                sum += prices[i]
 
-    return sum
+    def clac_score(self, indivisual):
+        dic = {}
+        dic['score0'] = 0  # value
+        dic['score1'] = 0  # weight
+        for ind in indivisual:
+            dic['score0'] += ind[0]
+            dic['score1'] += ind[1]
+            
+        return dic
+
+
+    def evaluate(self, pop):
+        fitness = []
+        for p in pop:
+            if not p.has_key('score0'):
+                # The indivisual made by crossover or mutation existed before
+                if self.fitness_master.has_key(str(p['param'])):
+                    p.update(self.fitness_master[str(p['param'])])
+                # The indivisual is the first
+                else:
+                    p.update(self.clac_score(p['param']))
+                fitness.append(p)
+            else:
+                fitness.append(p)
+
+        # Save fitness to all genaration dictinary
+        for fit in fitness:
+            param = fit['param']
+            self.fitness_master[str(param)] = {k:v for k,v in fit.items() if k!='param'}
+
+        # This generation fitness
+        df = pd.DataFrame(fitness)
+        df = df.sort(['score0', 'score1'], ascending=[False, True])
+
+        fitness = df.to_dict('records')
+        
+        return fitness
+
+
+    def mutate(self, parent):
+        ind_idx = int(math.floor(random.random()*len(parent)))
+        item_idx = random.choice(range(N_ITEMS))
+        child = copy.deepcopy(parent)
+        child[ind_idx] = self.items[item_idx]
+
+        return child
+
+
+    def crossover(self, parent1, parent2):
+        length = len(parent1)
+        r1 = int(math.floor(random.random()*length))
+        r2 = r1 + int(math.floor(random.random()*(length-r1)))
+        
+        child = copy.deepcopy(parent1)
+        child[r1:r2] = parent2[r1:r2]
+
+        return child
+
 
 if __name__ == "__main__":
-    populations = make_population()
-    print(evaluate(populations))
+    GA().main()
