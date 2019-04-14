e75655b27c69f1b298528d1a956f6e8a6cf6bc14
Daiki <nojiri1098@gmail.com>
2019-04-14 10:02:10 +0900

    Add make_population()

--- ga-knapsack.py
+++ ga-knapsack.py
@@ -1,4 +1,12 @@
 # -*- coding: utf-8 -*-
+import random
+import numpy as np
+
+MAX_WEIGHT = 60
+POPULATION_COUNT = 50
+GENERATION_COUNT = 30000
+CROSSING_RATE = 0.8
+MUTATION_RATE = 0.3
 
 weights = [
     9, 7, 8, 2, 10, 7, 7, 8, 5, 4, 7, 5, 7, 5, 9, 9, 9, 8, 8, 2, 7, 7, 9, 8, 4, 7,
@@ -9,6 +17,15 @@ prices = [
     14, 40, 36, 2, 28, 33, 40, 22, 2, 18, 22, 14, 22, 15, 22, 40, 7, 4, 21, 21, 28, 40, 4, 24, 21
 ]
 
+# 母集団を生成する
+def make_population():
+    populations = []
+
+    for i in range(0, POPULATION_COUNT):
+        individual = np.random.randint(0, 2, 50)
+        populations.append(individual) 
+
+    return populations
+
 if __name__ == "__main__":
-    print(weights)
-    print(prices)
+    populations = make_population()
